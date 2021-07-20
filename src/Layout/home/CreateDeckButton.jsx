b420633c842eb640b91a9d2955935e61f3208e27
Anon Vanichyanukroh <anonvanichyanukroh@gmail.com>
2021-07-20 00:11:51 -0700

    first commit

---
+++ src/Layout/home/CreateDeckButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateDeckButton({ createDeckClick }) {
+  return <button onClick={() => createDeckClick()}>Create Deck</button>;
+}
+
+export default CreateDeckButton;
